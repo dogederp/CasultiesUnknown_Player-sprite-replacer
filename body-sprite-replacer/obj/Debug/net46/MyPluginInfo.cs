e0df739b3e9db228e5695cf628878da59a989ec3
Karlo Poljančić <41737008+dogederp@users.noreply.github.com>
2026-03-17 09:32:36 +0100

    Delete body-sprite-replacer/obj directory

--- body-sprite-replacer/obj/Debug/net46/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace body_sprite_replacer
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "body_sprite_replacer";
-        public const string PLUGIN_NAME = "body_sprite_replacer";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
